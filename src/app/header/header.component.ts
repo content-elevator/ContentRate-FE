fe05fb50b1c67251251cab7a40c55a64af769823
tiboth <37446840+tiboth@users.noreply.github.com>
2020-04-13 14:12:12 +0300

    added header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../shared/service/authentication.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(private authService: AuthenticationService) { }
+
+  ngOnInit(): void {
+  }
+
+  logout(){
+    console.log('logging out');
+    this.authService.logout();
+  }
+
+}
