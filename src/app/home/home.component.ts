b4048c7eb153550c5ba86c4e0715d6589bb1edc2
Alin Pop <alinpop97@gmail.com>
2020-05-06 15:57:02 +0300

    update home page with input form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {first} from 'rxjs/operators';
+import {AuthenticationService} from '../shared/service/authentication.service';
+import {UtilService} from '../shared/service/util.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  rateForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private utilService: UtilService
+  ) {
+    // redirect to home if already logged in
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  ngOnInit() {
+    this.rateForm = this.formBuilder.group({
+      query: ['', Validators.required],
+      url: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.rateForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.rateForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService.login(this.f.query.value, this.f.url.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.loading = false;
+        });
   }
 
 }
