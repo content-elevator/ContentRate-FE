514af06961184b622b24275ee5581a505c730734
Dan <daniftincaalexandru@gmail.com>
2020-05-31 18:49:50 +0300

    deployment

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/content-rate-fe'));
+app.use(express.static(__dirname + '/dist/ContentRate-FE'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/content-rate-fe/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ContentRate-FE/index.html'));
 });
 
 // default Heroku PORT
